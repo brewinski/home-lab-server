0d1b34f9dbefe48d5fea43790270426261cc2035
Chris Brewin <cbrewin93@gmail.com>
2023-12-06 10:02:16 +1000

    upload in progress work to ensure it's not lost

--- src/scratch-pad/cmd/main.go
+++ src/scratch-pad/cmd/main.go
@@ -7,13 +7,13 @@ import (
 
 func main() {
 	fmt.Print("Starting application...")
-	limit := make(chan struct{}, 50)
+	limit := make(chan struct{}, 5)
 
 	for {
 		limit <- struct{}{}
 		// httpGetRequest to `http://192.168.0.201/dashboard/#/udp/routers`
 		go func() {
-			resp, err := http.Get("http://192.168.0.201/dashboard/#/udp/routers")
+			resp, err := http.Get("http://192.168.0.208")
 			fmt.Printf("%v, %v", resp, err)
 			<-limit
 		}()
