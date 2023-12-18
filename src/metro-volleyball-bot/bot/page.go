5e7e9ca58302c96cff5e0de06f35d6c06973004b
Chris Brewin <chris.brewin@canstar.com.au>
2023-12-18 15:00:27 +1000

    implement tests for planned replacement code.

--- src/metro-volleyball-bot/bot/page.go
+++ src/metro-volleyball-bot/bot/page.go
@@ -1,10 +1,48 @@
 package bot
 
 import (
+	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 )
+
+type Web struct {
+	client *http.Client
+}
+
+type WebConfig struct {
+	Client *http.Client
+}
+
+func NewWeb(config WebConfig) *Web {
+	return &Web{
+		config.Client,
+	}
+}
+
+// MonitorPage will monitor a specific page for changes.
+func (w *Web) MonitorPage(pageUrl string) (string, error) {
+	response, err := w.client.Get(pageUrl)
+	if err != nil {
+		return "", fmt.Errorf("MonitorPage() request failed, got: %w", err)
+	}
+
+	if response.StatusCode < 200 || response.StatusCode > 299 {
+		return "", fmt.Errorf("MonitorPage() response status code, got: %d", response.StatusCode)
+	}
+
+	defer response.Body.Close()
+
+	respBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	slog.Info("page response", "status", response.Status, "content-length", response.ContentLength)
+
+	return string(respBytes), nil
+}
 
 // ping the page and check if anything has changed.
 // it will return false if the page hasn't been checked in the past
--- src/metro-volleyball-bot/bot/page_test.go
+++ src/metro-volleyball-bot/bot/page_test.go
@@ -1,6 +1,10 @@
 package bot
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestMonitorPage(t *testing.T) {
 	type args struct {
@@ -33,6 +37,64 @@ func TestMonitorPage(t *testing.T) {
 			if (err != nil) != tt.wantErr {
 				t.Errorf("MonitorPage() error = %v, wantErr %v", err, tt.wantErr)
 				return
+			}
+		})
+	}
+}
+
+func TestWeb_MonitorPage(t *testing.T) {
+	type fields struct {
+		client  *http.Client
+		handler http.HandlerFunc
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "TestWeb_MonitorPage will return the page response",
+			fields: fields{
+				client: &http.Client{},
+				handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.Write([]byte("OK"))
+
+					w.WriteHeader(http.StatusOK)
+				}),
+			},
+			want:    "OK",
+			wantErr: false,
+		},
+		{
+			name: "TestWeb_MonitorPage will return an error if the page does not exist",
+			fields: fields{
+				client: &http.Client{},
+				handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(http.StatusNotFound)
+				}),
+			},
+			want:    "",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testServer := httptest.NewServer(tt.fields.handler)
+
+			defer testServer.Close()
+
+			w := &Web{
+				client: tt.fields.client,
+			}
+
+			got, err := w.MonitorPage(testServer.URL)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Web.MonitorPage() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Web.MonitorPage() = %v, want %v", got, tt.want)
 			}
 		})
 	}
